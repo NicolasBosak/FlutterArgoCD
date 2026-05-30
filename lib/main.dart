20c29c5ccb86f6f039daaaf484bfdc6343c447c2
Sebastian Casamen <sebastian.casamen@netbyec.com>
2026-05-29 20:40:22 -0500

    Migrate to ArgoCD GitOps, use Docker Hub, and update app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter + ArgoCD GitOps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
